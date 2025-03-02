10ead440b43196244a68c4985d918be67fafe765
Aashi <161113739+Aashi-ghub@users.noreply.github.com>
2025-03-03 01:13:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(
     cookie: {
        secure:true, // Enable in production with HTTPS
        httpOnly:true,
-       sameSite: "lax",
+       sameSite: "none",
        maxAge: 1000 * 60 * 60 * 24,
     },
   })
