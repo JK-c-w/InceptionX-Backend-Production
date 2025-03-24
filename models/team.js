70488de4fdbfa3da4b33501cf772ef83f8055154
JK-c-w <jyotikhetwal29@gmail.com>
2025-03-24 23:44:17 +0530

    Add isLeader field to member schema and remove socialLink from team model

--- models/team.js
+++ models/team.js
@@ -4,14 +4,13 @@ const memberSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
-  socialLink: { type: String, required: false },
   college: { type: String, required: true },
+  isLeader: { type: Boolean, required: true},
 });
 
 const teamSchema = new mongoose.Schema({
   teamName: { type: String, required: true, unique: true },
   teamSize: { type: Number, required: true },
-  // termsAccepted: { type: Boolean, required: true },
   members: [memberSchema],
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   createdAt: { type: Date, default: Date.now },
